e70299c74b42d67bbaa037b26acadc2032e08bc5
Aarush_0312 <aarushkumarchoubey@gmail.com>
2025-04-22 16:41:30 +0000

    added dynamic layout for job webpages

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ def listing_jobs():
 @app.route('/job/<id>')
 def showing_job(id):
     job=load_job_from_db(id)
-    return jsonify(job)
+    if not job:
+        return "Not Found",404
+    return render_template('jobwebpage.html',job=job)
 
 
 
